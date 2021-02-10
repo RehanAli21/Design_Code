43c296e8500b5a4a7bf0e29cc0778ee0737035f9
Rehan Ali <rehan.shah70@yahoo.com>
2021-02-10 16:41:48 +0500

    Slider / Addbar Added.

--- client/src/App.js
+++ client/src/App.js
@@ -1,34 +1,21 @@
 import React, { useState } from 'react'
 import Nav from './Components/Nav'
+import Slider from './Components/Slider'
 
 const App = () => {
 	const [layout, setLayout] = useState('X-large')
+	const [addBarVisible, setAddBarVisible] = useState(false)
 
 	return (
 		<div>
 			<Nav layout={layout} setLayout={setLayout} />
-			<div id='create' className='create'>
+			<div
+				onClick={() => setAddBarVisible(!addBarVisible)}
+				id='create'
+				className='create'>
 				&#43;
 			</div>
-			<div id='create-options' className='create-options'>
-				<div className='slider-up' id='slider_up'>
-					&#10095; &#10094; &#9650;
-				</div>
-				<div className='options'>
-					<div className='option'>Navbar</div>
-					<div className='option'>Row</div>
-					<div className='option'>Col</div>
-					<div className='option'>Form</div>
-					<div className='option'>Inputs</div>
-					<div className='option'>Buttons</div>
-					<div className='option'>Image</div>
-					<div className='option'>Tables</div>
-					<div className='option'>lists</div>
-				</div>
-				<div className='slider-down' id='slider_up'>
-					&#9660;
-				</div>
-			</div>
+			<Slider visible={addBarVisible} />
 			<main
 				style={{
 					width:
@@ -40,7 +27,12 @@ const App = () => {
 							? '960px'
 							: '100%',
 					boxShadow: layout !== 'X-large' ? '2px 2px 20px grey' : ''
-				}}></main>
+				}}>
+				<h1>Hello Rehan!</h1>
+				<h1>Hello Rehan!</h1>
+				<h1>Hello Rehan!</h1>
+				<h1>Hello Rehan!</h1>
+			</main>
 		</div>
 	)
 }
