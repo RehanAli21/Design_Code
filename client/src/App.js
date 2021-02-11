86c3b1afd4b633b307561df3518438075077f807
Rehan Ali <rehan.shah70@yahoo.com>
2021-02-11 13:07:56 +0500

    Remaking UI

--- client/src/App.js
+++ client/src/App.js
@@ -1,40 +1,7 @@
-import React, { useState } from 'react'
-import Nav from './Components/Nav'
-import Slider from './Components/Slider'
+import React from 'react'
 
 const App = () => {
-	const [layout, setLayout] = useState('X-large')
-	const [addBarVisible, setAddBarVisible] = useState(false)
-
-	return (
-		<div>
-			<Nav layout={layout} setLayout={setLayout} />
-			<div
-				onClick={() => setAddBarVisible(!addBarVisible)}
-				id='create'
-				className='create'>
-				&#43;
-			</div>
-			<Slider visible={addBarVisible} />
-			<main
-				style={{
-					width:
-						layout === 'Small'
-							? '540px'
-							: layout === 'Medium'
-							? '720px'
-							: layout === 'Large'
-							? '960px'
-							: '100%',
-					boxShadow: layout !== 'X-large' ? '2px 2px 20px grey' : ''
-				}}>
-				<h1>Hello Rehan!</h1>
-				<h1>Hello Rehan!</h1>
-				<h1>Hello Rehan!</h1>
-				<h1>Hello Rehan!</h1>
-			</main>
-		</div>
-	)
+	return <div></div>
 }
 
 export default App
