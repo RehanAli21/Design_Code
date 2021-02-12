161c111b96ea9bd4f56a12f0f2c48f4356b82ed8
Rehan Ali <rehan.shah70@yahoo.com>
2021-02-12 12:35:04 +0500

    Navbar update.
    Now in navbar pages will be showed dynamically and width is changing with small, medium etc.

--- client/src/Components/Contexts/PagesContext.js
+++ client/src/Components/Contexts/PagesContext.js
@@ -1,10 +1,26 @@
-import React, { createContext } from 'react'
+import React, { createContext, useState } from 'react'
 
 export const PagesContext = createContext()
 
 export const PagesProvider = props => {
+	const [pages, setPages] = useState(['home'])
+	const [activePage, setActivePage] = useState('home')
+	const [pageWidths, setPageWidths] = useState([
+		'small',
+		'medium',
+		'large',
+		'x-large'
+	])
+	const [activePageWidth, setActivePageWidth] = useState('small')
 	return (
-		<PagesContext.Provider value={'hi'}>
+		<PagesContext.Provider
+			value={[
+				pages,
+				activePage,
+				setActivePage,
+				activePageWidth,
+				setActivePageWidth
+			]}>
 			{props.children}
 		</PagesContext.Provider>
 	)
--- client/src/Components/Navbar.js
+++ client/src/Components/Navbar.js
@@ -1,11 +1,39 @@
-import React from 'react'
-// import { PagesContext } from './Contexts/PagesContext'
+import React, { useContext } from 'react'
+import { PagesContext } from './Contexts/PagesContext'
 
 const Navbar = () => {
-	// const value = useContext(PagesContext)
+	const [
+		pages,
+		activePage,
+		setActivePage,
+		activePageWidth,
+		setActivePageWidth
+	] = useContext(PagesContext)
+
+	const toCapitalize = s => s.charAt(0).toUpperCase() + s.slice(1, s.length)
+
 	return (
 		<nav>
-			<h3>Home</h3>
+			<h1>{'<'}</h1>
+			<div>
+				<select
+					onChange={e =>
+						setActivePageWidth(e.target.value.toLowerCase())
+					}>
+					<option>Small</option>
+					<option>Medium</option>
+					<option>Large</option>
+					<option>x-large</option>
+				</select>
+			</div>
+			<div>
+				<select
+					onChange={e => setActivePage(e.target.value.toLowerCase())}>
+					{pages.map(page => (
+						<option key={page}>{toCapitalize(page)}</option>
+					))}
+				</select>
+			</div>
 		</nav>
 	)
 }
