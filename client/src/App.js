856c472f9e11650b41bcd9165c21e7814f91313e
Rehan Ali <rehan.shah70@yahoo.com>
2021-02-14 13:05:14 +0500

    Now main-div move only on when 'm' is pressed.

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Navbar from './Components/Navbar'
 import Toolbar from './Components/Toolbar'
 import Propertiesbar from './Components/Propertiesbar'
@@ -7,15 +7,28 @@ import { PagesProvider } from './Components/Contexts/PagesContext'
 import { TransformWrapper, TransformComponent } from 'react-zoom-pan-pinch'
 
 const App = () => {
+	const [move, setMove] = useState(true)
+
+	document.addEventListener('keydown', e =>
+		e.key.toLowerCase() === 'm' ? setMove(false) : null
+	)
+	document.addEventListener('keyup', e =>
+		e.key.toLowerCase() === 'm' ? setMove(true) : null
+	)
+
 	return (
 		<PagesProvider>
 			<div>
 				<Navbar />
 				<div className='three-section'>
 					<Toolbar />
-					<div className='main-div' id='main-div'>
+					<div
+						style={{ cursor: move ? 'default' : 'grab' }}
+						className='main-div'
+						id='main-div'>
 						<TransformWrapper
 							options={{
+								disabled: move,
 								minScale: 0.5,
 								maxScale: 2,
 								limitToBounds: false
