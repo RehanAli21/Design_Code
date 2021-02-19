6caca720898e4f8939de0606a42a6746ce674aba
Rehan Ali <rehan.shah70@yahoo.com>
2021-02-19 12:45:25 +0500

    Deleting from layers items done. (for now)

--- client/src/Components/Layers.js
+++ client/src/Components/Layers.js
@@ -3,14 +3,41 @@ import uuid from 'react-uuid'
 import { PageContext } from './Contexts/PageContext'
 
 const Layers = () => {
-	const { pages, activePage, activeElement, setActiveElement } = useContext(
-		PageContext
-	)
+	const {
+		pages,
+		setPages,
+		activePage,
+		activeElement,
+		setActiveElement
+	} = useContext(PageContext)
 
 	const toCapitalize = s => s.charAt(0).toUpperCase() + s.slice(1, s.length)
 
 	const changeActiveElement = e => {
 		setActiveElement(e.target.id)
+	}
+
+	const deleteMe = e => {
+		const id = e.target.id.split('_')[0]
+
+		const temp = Object.assign({}, pages)
+
+		temp[activePage] = deleteIt(temp[activePage], id)
+		setPages(temp)
+	}
+
+	const deleteIt = (arr, id) => {
+		const e = []
+		console.log(arr)
+		arr.forEach(a => {
+			if (a[1].id === id) return
+
+			if (a[2].length > 0) deleteIt(a)
+
+			e.push(a)
+		})
+
+		return e
 	}
 
 	const showLayers = data => {
@@ -27,7 +54,12 @@ const Layers = () => {
 								onClick={changeActiveElement}>
 								{e[0]}
 							</p>
-							<button className='layer-delete'>D</button>
+							<button
+								id={e[1].id + '_btn'}
+								onClick={deleteMe}
+								className='layer-delete'>
+								X
+							</button>
 							{showLayers(e[2])}
 						</li>
 					)
