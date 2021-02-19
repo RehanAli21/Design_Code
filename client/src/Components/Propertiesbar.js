71b3206846d654cf9059f72a42c0a449bd7debac
Rehan Ali <rehan.shah70@yahoo.com>
2021-02-19 23:15:35 +0500

    styles list shown in propertiesbar

--- client/src/Components/Propertiesbar.js
+++ client/src/Components/Propertiesbar.js
@@ -1,4 +1,5 @@
-import React, { useState, useContext } from 'react'
+import React, { useState, useContext, useEffect } from 'react'
+import uuid from 'react-uuid'
 import { PageContext } from './Contexts/PageContext'
 
 const Propertiesbar = () => {
@@ -7,24 +8,67 @@ const Propertiesbar = () => {
 	const [large, setLarge] = useState({})
 	const [xlarge, setXlarge] = useState({})
 
-	const { pages, setPages, activePage, activeElement } = useContext(
-		PageContext
-	)
+	const { pages, activePage, activeElement } = useContext(PageContext)
+
+	useEffect(() => {
+		showProperties(pages[activePage], activeElement)
+	}, [pages, activePage, activeElement])
 
 	const showProperties = (arr, id) => {
 		arr.forEach(e => {
 			if (e[1].id === id) {
-				alert('found in property')
+				setSmall(e[1].styles.small)
+				setMedium(e[1].styles.medium)
+				setLarge(e[1].styles.large)
+				setXlarge(e[1].styles.xlarge)
+				return true
 			} else if (e[2].length > 0) {
-				showProperties(e[2], id)
+				if (showProperties(e[2], id)) return
 			}
 		})
+	}
+
+	const showList = () => {
+		return (
+			<ul>
+				<li>
+					<h5>Small</h5>
+					<ul>{mapList(small)}</ul>
+				</li>
+				<li>
+					<h5>Medium</h5>
+					<ul>{mapList(medium)}</ul>
+				</li>
+				<li>
+					<h5>Large</h5>
+					<ul>{mapList(large)}</ul>
+				</li>
+				<li>
+					<h5>X Large</h5>
+					<ul>{mapList(xlarge)}</ul>
+				</li>
+			</ul>
+		)
+	}
+	const mapList = obj => {
+		const temp = []
+		for (const key in obj) {
+			temp.push(
+				<li
+					key={uuid()}
+					style={{ width: '100%', backgroundColor: 'red' }}>
+					<span style={{ width: '75%' }}>{key}</span>
+					<input style={{ width: '25%' }} type='text' />
+				</li>
+			)
+		}
+		return temp
 	}
 
 	return (
 		<div className='propertybar'>
 			<p>Properties</p>
-			{showProperties(pages[activePage], activeElement)}
+			{showList()}
 		</div>
 	)
 }
