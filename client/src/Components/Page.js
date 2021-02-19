1182bc698f8d4de4bc35aff1b061d878fe3322c3
Rehan Ali <rehan.shah70@yahoo.com>
2021-02-19 18:40:08 +0500

    Elements are inserting into main div

--- client/src/Components/Page.js
+++ client/src/Components/Page.js
@@ -8,15 +8,33 @@ const Page = () => {
 	const { pages, activePage } = useContext(PageContext)
 	const toCapitalize = s => s.charAt(0).toUpperCase() + s.slice(1, s.length)
 
+	const showElements = arr => {
+		const temp = []
+		if (!arr) return null
+
+		arr.forEach(e => {
+			if (e[2].length > 0)
+				temp.push(
+					React.createElement(
+						e[0],
+						{ key: uuid() },
+						showElements(e[2])
+					)
+				)
+
+			temp.push(React.createElement(e[0], { key: uuid() }, null))
+		})
+		return temp
+	}
+
 	return (
 		<div
 			className='full-width'
 			style={{ width: window.screen.width / 1.4 }}>
 			<div>
-				<Resizable
-					className='pages-div'
-					width={720}
-					id={uuid()}></Resizable>
+				<Resizable className='pages-div' width={720} id={activePage}>
+					{showElements(pages[activePage])}
+				</Resizable>
 			</div>
 		</div>
 	)
