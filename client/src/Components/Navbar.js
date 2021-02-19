107e8e44d606e47e56154023cffc4fed3304f963
Rehan Ali <rehan.shah70@yahoo.com>
2021-02-19 18:40:51 +0500

    Select converted into dropdown

--- client/src/Components/Navbar.js
+++ client/src/Components/Navbar.js
@@ -3,10 +3,11 @@ import uuid from 'react-uuid'
 import { PageContext } from './Contexts/PageContext'
 
 const Navbar = () => {
-	const { pages, setPages, activePage, setActivePage, setWidth } = useContext(
+	const { pages, setPages, activePage, setActivePage } = useContext(
 		PageContext
 	)
 	const [pageName, setPageName] = useState('')
+	const [show, setShow] = useState(false)
 
 	const toCapitalize = s => s.charAt(0).toUpperCase() + s.slice(1, s.length)
 
@@ -24,15 +25,21 @@ const Navbar = () => {
 	}
 
 	const changeActivePage = e => {
-		if (e.target.value !== activePage)
-			setActivePage(e.target.value.toLowerCase())
+		if (e.target.innerText !== activePage)
+			setActivePage(e.target.innerText.toLowerCase())
+
+		setShow(!show)
 	}
 
 	const showPages = () => {
 		const temp = []
 
 		for (let i in pages) {
-			temp.push(<option key={uuid()}>{toCapitalize(i)}</option>)
+			temp.push(
+				<li onClick={changeActivePage} key={uuid()}>
+					{toCapitalize(i)}
+				</li>
+			)
 		}
 		return temp
 	}
@@ -41,7 +48,12 @@ const Navbar = () => {
 		<nav>
 			<h1>{'<'}</h1>
 			<div>
-				<select onChange={changeActivePage}>{showPages()}</select>
+				<p onClick={() => setShow(!show)}>
+					{toCapitalize(activePage)} <span>V</span>
+				</p>
+				<ul style={{ display: show ? 'block' : 'none' }}>
+					{showPages()}
+				</ul>
 			</div>
 			<div className='addpage-div'>
 				<input
