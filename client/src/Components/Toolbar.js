283b61575cadb6a42b5f321a89b221afea010a79
Rehan Ali <rehan.shah70@yahoo.com>
2021-03-04 21:27:05 +0500

    Comments added into Toolbar Component

--- client/src/Components/Toolbar.js
+++ client/src/Components/Toolbar.js
@@ -2,21 +2,30 @@ import React, { useContext } from 'react'
 import { PageContext } from './Contexts/PageContext'
 import Div from './Tools/Div'
 
+//This component is for showing list of elemenets,
+//which can be added.
 const Toolbar = () => {
 	const { pages, setPages, activePage, activeElement } = useContext(
 		PageContext
 	)
 
+	//For Getting unique string for ids of elements
 	const uniqueString = () =>
 		Math.random().toString(36).substring(2, 15) +
 		Math.random().toString(36).substring(2, 15)
 
+	//For finding the parent element,
+	//in which elements will be added, using recursion
 	const findAndInsert = (children, id, element) => {
+		//Iterating each child in children array
 		children.forEach(child => {
+			//if parent found by id, then insert element into children
 			if (child[1].id === id) {
 				child[2].push(element)
 				return children
-			} else if (child[2].length > 0) {
+			}
+			//else if there is children, then find into children recusively
+			else if (child[2].length > 0) {
 				findAndInsert(child[2], id, element)
 			}
 		})
