39f5124b64bb85ed9218b5d3af1adbf31f712bbf
Rehan Ali <rehan.shah70@yahoo.com>
2021-03-06 22:29:12 +0500

    Upper Layer Component updated.
    Upper layer is overlaying on activeElement properly, now changing width, height and margins left.

--- client/src/Components/Page.js
+++ client/src/Components/Page.js
@@ -4,7 +4,12 @@ import uuid from 'react-uuid'
 
 let oldx = 0
 let oldy = 0
-let prevActiveElement = ''
+let l = 0
+let t = 0
+let w = 0
+let h = 0
+let parentWidth = 0
+let parentHeight = 0
 //This compoenent controls page.
 const Page = () => {
 	const { pages, activePage, width, activeElement, render } = useContext(
@@ -16,7 +21,6 @@ const Page = () => {
 	const [h, setH] = useState(0)
 	const [parentWidth, setParentWidth] = useState(0)
 	const [parentHeight, setParentHeight] = useState(0)
-	let parent = ''
 
 	//For showing elements into a div which acts
 	//as body element/tag, using recursion
@@ -62,92 +66,100 @@ const Page = () => {
 		} else if (e.pageX < oldx) {
 			if (l >= 0) setL(l - 1)
 		}
-
 		if (e.pageY > oldy) {
 			if (t < parentHeight - h) setT(t + 1)
 		} else if (e.pageY < oldy) {
 			if (t >= 0) setT(t - 1)
 		}
-
 		oldy = e.pageY
 		oldx = e.pageX
 	}
 
 	const widthHeight = e => {
-		if (e.pageX > oldx) {
-			if (width < parentWidth) setW(w + 1)
-		} else if (e.pageX < oldx) {
-			if (width >= 0) setW(w - 1)
-		}
-
-		if (e.pageY > oldy) {
-			if (h < parentHeight) setH(h + 1)
-		} else if (e.pageY < oldy) {
-			if (h >= 0) setH(h - 1)
-		}
-
-		oldy = e.pageY
-		oldx = e.pageX
+		// if (e.pageX > oldx) {
+		// 	if (width < parentWidth) setW(w + 1)
+		// } else if (e.pageX < oldx) {
+		// 	if (width >= 0) setW(w - 1)
+		// }
+		// if (e.pageY > oldy) {
+		// 	if (h < parentHeight) setH(h + 1)
+		// } else if (e.pageY < oldy) {
+		// 	if (h >= 0) setH(h - 1)
+		// }
+		// oldy = e.pageY
+		// oldx = e.pageX
 	}
 
 	const HeightChange = e => {
-		if (e.pageY > oldy) {
-			if (h < parentHeight) setH(h + 1)
-		} else if (e.pageY < oldy) {
-			if (h >= 0) setH(h - 1)
-		}
-
-		oldy = e.pageY
+		// if (e.pageY > oldy) {
+		// 	if (h < parentHeight) setH(h + 1)
+		// } else if (e.pageY < oldy) {
+		// 	if (h >= 0) setH(h - 1)
+		// }
+		// oldy = e.pageY
 	}
 
 	const WidthChange = e => {
-		if (e.pageX > oldx) {
-			if (w < parentWidth) setW(w + 1)
-		} else if (e.pageX < oldx) {
-			if (w >= 0) setW(w - 1)
-		}
-		oldx = e.pageX
+		// if (e.pageX > oldx) {
+		// 	if (w < parentWidth) setW(w + 1)
+		// } else if (e.pageX < oldx) {
+		// 	if (w >= 0) setW(w - 1)
+		// }
+		// oldx = e.pageX
 	}
 
+	const changeStatesForUpperLayer = (p, e) => {
+		setParentWidth(p.clientWidth)
+		setParentHeight(p.clientHeight)
+		setL(e.offsetLeft)
+		setT(e.offsetTop)
+		setW(e.clientWidth)
+		setH(e.clientHeight)
+	}
+
+	const addEvent = (e, func) =>
+		document.getElementById(e).addEventListener('drag', func)
+
 	useEffect(() => {
-		findParent(activePage, pages[activePage], activeElement)
-		if (parent !== '' && activeElement !== '') {
-			let p = document.getElementById(parent)
+		if (activeElement !== '' && activeElement !== activePage) {
 			let e = document.getElementById(activeElement)
+			if (e !== null) {
+				let p = document.getElementById(activeElement).parentElement
 
-			console.log('parnet wifht: ', p.clientWidth)
-			console.log('parnet height: ', p.clientHeight)
-			console.log('e client left: ', e.clientLeft)
-			console.log('e client Top: ', e.clientTop)
-			console.log('e width: ', e.clientWidth)
-			console.log('e height: ', e.clientHeight)
-			console.log('e margin left: ', e.style.marginLeft)
-			console.log('e margin top: ', e.style.marginTop)
-			console.log('e offsetleft: ', e.offsetLeft)
-			console.log('e offsetTop: ', e.offsetTop)
-			setParentWidth(p.clientWidth)
-			setParentHeight(p.clientHeight)
-			setL(e.offsetLeft)
-			setT(e.offsetTop)
-			setW(e.clientWidth)
-			setH(e.clientHeight)
+				console.log('parnet wifht: ', p.clientWidth)
+				console.log('parnet height: ', p.clientHeight)
+				console.log('e client left: ', e.clientLeft)
+				console.log('e client Top: ', e.clientTop)
+				console.log('e width: ', e.clientWidth)
+				console.log('e height: ', e.clientHeight)
+				console.log('e margin left: ', e.style.marginLeft)
+				console.log('e margin top: ', e.style.marginTop)
+				console.log('e offsetleft: ', e.offsetLeft)
+				console.log('e offsetTop: ', e.offsetTop)
 
-			const upperLayer = document.getElementById('ul-container')
+				changeStatesForUpperLayer(p, e)
+				console.log(l, t, w, h, parentWidth, parentHeight)
 
-			if (upperLayer !== null) {
-				upperLayer.remove()
-			}
+				const upperLayer = document.getElementById('ul-container')
 
-			e.innerHTML += `<div id='ul-container' class='ul-container'>
-				<div
-					style="width: ${w}px; height: ${h}px; margin-left: ${l}px; margin-top: ${t}px;"
+				if (upperLayer !== null) {
+					upperLayer.remove()
+				}
+
+				const element = document.createElement('div')
+
+				element.innerHTML += `<div id='ul-container' class='ul-container'>
+			<div
+			style="width: ${e.clientWidth}px; height: ${e.clientHeight}px; margin-left: ${
+					e.offsetLeft
+				}px; margin-top: ${e.offsetTop}px;"
 					class='upper-layer'
 					id='upper-layer'>
 					<div
 						draggable='true'
 						id='mover'
 						class='mover'
-						style="left: ${(w - 10) / 2.1}px">
+						style="left: ${(e.clientWidth - 10) / 2.1}px">
 						<div class='line'></div>
 					</div>
 					<div
@@ -169,42 +181,42 @@ const Page = () => {
 					<div
 						draggable='true'
 						class='top'
-						style="left: ${(w - 10) / 2.1}px"></div>
+						id='upperlayer-t'
+						style="left: ${(e.clientWidth - 10) / 2.1}px"></div>
 					<div
 						draggable='true'
 						class='bottom'
-						style="left: ${(w - 10) / 2.1}px"></div>
+						id='upperlayer-b'
+						style="left: ${(e.clientWidth - 10) / 2.1}px"></div>
 					<div
 						draggable='true'
 						class='left'
-						style="top: ${(h - 10) / 2.1}px"></div>
+						id='upperlayer-l'
+						style="top: ${(e.clientHeight - 10) / 2.1}px"></div>
 					<div
 						draggable='true'
 						class='right'
-						style="top: ${(h - 10) / 2.1}px"></div>
+						id='upperlayer-r'
+						style="top: ${(e.clientHeight - 10) / 2.1}px"></div>
 				</div>
 			</div>`
 
-			document.getElementById('mover').addEventListener('drag', move)
+				p.appendChild(element)
 
-			document.getElementById('tl').addEventListener('drag', widthHeight)
+				addEvent('mover', move)
 
-			document.getElementById('tr').addEventListener('drag', widthHeight)
-			document.getElementById('bl').addEventListener('drag', widthHeight)
-			document.getElementById('br').addEventListener('drag', widthHeight)
-		}
-	}, [activePage, pages, activeElement])
+				addEvent('tl', widthHeight)
+				addEvent('tr', widthHeight)
+				addEvent('bl', widthHeight)
+				addEvent('br', widthHeight)
 
-	const findParent = (parentId, arr, id) => {
-		arr.forEach(element => {
-			if (element[1].id === id) {
-				parent = parentId
-				return true
-			} else if (element[2].length > 0) {
-				if (findParent(element[1].id, element[2], id)) return true
+				addEvent('upperlayer-l', WidthChange)
+				addEvent('upperlayer-r', WidthChange)
+				addEvent('upperlayer-b', HeightChange)
+				addEvent('upperlayer-t', HeightChange)
 			}
-		})
-	}
+		}
+	}, [activePage, pages, activeElement, width, render])
 
 	return (
 		<div
