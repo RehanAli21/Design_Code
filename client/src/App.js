5d1ec74554492529834f79067df96279ff5995c5
Rehan Ali Shah <rehan.shah70@yahoo.com>
2021-03-12 17:39:10 +0500

    making own zoom and dragable functionality.
    zooming is enabled by keyboard by press z and arrow up zoom in and by press z and pressdown zoom out.

--- client/src/App.js
+++ client/src/App.js
@@ -1,19 +1,13 @@
-import React, { useState } from 'react'
+import React from 'react'
 import Navbar from './Components/Navbar'
 import Toolbar from './Components/Toolbar'
 import Propertiesbar from './Components/Propertiesbar'
 import Page from './Components/Page'
 import Layers from './Components/Layers'
-import { TransformWrapper, TransformComponent } from 'react-zoom-pan-pinch'
 
 import { PageProvider } from './Components/Contexts/PageContext'
 
 const App = () => {
-	const [move, setMove] = useState(true)
-
-	document.addEventListener('keydown', e => (e.key.toLowerCase() === 'z' ? setMove(false) : null))
-	document.addEventListener('keyup', e => (e.key.toLowerCase() === 'z' ? setMove(true) : null))
-
 	return (
 		<div>
 			<PageProvider>
@@ -21,20 +15,7 @@ const App = () => {
 				<div className='three-section'>
 					<Toolbar />
 					<Layers />
-					<div style={{ cursor: move ? 'default' : 'grab' }} className='main-div' id='main-div'>
-						<TransformWrapper
-							options={{
-								disabled: move,
-								minScale: 0.25,
-								maxScale: 4,
-								limitToBounds: false
-							}}
-							doubleClick={{ mode: 'reset' }}>
-							<TransformComponent>
-								<Page />
-							</TransformComponent>
-						</TransformWrapper>
-					</div>
+					<Page />
 					<Propertiesbar />
 				</div>
 			</PageProvider>
--- client/src/Components/Page.js
+++ client/src/Components/Page.js
@@ -1,10 +1,33 @@
-import React, { useContext, useEffect, useState } from 'react'
+import React, { useContext, useState } from 'react'
 import { PageContext } from './Contexts/PageContext'
 import uuid from 'react-uuid'
 
+let z = false
+let scale = 0.8
 //This compoenent controls page.
 const Page = () => {
 	const { pages, activePage, width, render } = useContext(PageContext)
+	const [move, setMove] = useState(false)
+
+	document.addEventListener('keydown', e => {
+		if (e.key === 'z') {
+			z = true
+			setMove(true)
+		}
+		if (e.key === 'ArrowUp' && scale < 2 && z === true) {
+			scale += 0.01
+			const ele = document.querySelector('.pages-div')
+			if (ele) ele.style.transform = `scale(${scale})`
+		} else if (e.key === 'ArrowDown' && scale > 0.5 && z === true) {
+			scale -= 0.01
+			const ele = document.querySelector('.pages-div')
+			if (ele) ele.style.transform = `scale(${scale})`
+		}
+	})
+
+	document.addEventListener('keyup', e => {
+		if (e.key === 'z') z = false
+	})
 
 	//For showing elements into a div which acts
 	//as body element/tag, using recursion
@@ -28,7 +51,7 @@ const Page = () => {
 						key: uuid(),
 						id: e[1].id,
 						style:
-							width < 540 ? e[1].styles.small : width < 720 ? e[1].styles.medium : width < 960 ? e[1].styles.large : e[1].styles.xlarge
+							width < 540 ? e[1].styles.small : width < 720 ? e[1].styles.medium : width < 960 ? e[1].styles.large : e[1].styles.xlarge,
 					},
 					e[2].length > 0 ? showElements(e[2]) : null
 				)
@@ -39,18 +62,16 @@ const Page = () => {
 	}
 
 	return (
-		<div className='full-width' style={{ width: window.screen.width / 1.4 }}>
-			<div className={render ? 'ok' : ''}>
-				<div
-					className='pages-div'
-					id={activePage}
-					style={{
-						width: `${width}px`,
-						minWidth: '300px',
-						marginLeft: width < 720 ? '15%' : width < 1000 ? '10%' : width < 1500 ? '5%' : '0%'
-					}}>
-					{showElements(pages[activePage])}
-				</div>
+		<div className={render ? 'ok main-div' : 'main-div'}>
+			<div
+				className='pages-div'
+				id={activePage}
+				style={{
+					width: `${width}px`,
+					minWidth: '300px',
+					marginLeft: width < 720 ? '15%' : width < 1000 ? '10%' : width < 1500 ? '5%' : '0%',
+				}}>
+				{showElements(pages[activePage])}
 			</div>
 		</div>
 	)
