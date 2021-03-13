8ff3e70dc398f916454e8852ab8dab8bc8a82cdb
Rehan Ali Shah <rehan.shah70@yahoo.com>
2021-03-13 11:25:41 +0500

    DivProperties update
    UI is ready for rows and columns except the it does not show num of rows and cols dynamically.

--- client/src/Components/Properties/DivProperties.js
+++ client/src/Components/Properties/DivProperties.js
@@ -2,9 +2,37 @@ import React, { useState } from 'react'
 
 const DivProperties = () => {
 	const [showRow, setShowRow] = useState(false)
-	const [rowNum, setRowNum] = useState([0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0])
+	const [rowsNum, setRowNum] = useState(0)
+	const [rowValues, setRowValues] = useState([
+		[0, 'px'],
+		[0, 'px'],
+		[0, 'px'],
+		[0, 'px'],
+		[0, 'px'],
+		[0, 'px'],
+		[0, 'px'],
+		[0, 'px'],
+		[0, 'px'],
+		[0, 'px'],
+		[0, 'px'],
+		[0, 'px'],
+	])
 	const [showCol, setShowCol] = useState(false)
-	const [colNum, setColNum] = useState([0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0])
+	const [colsNum, setColsNum] = useState(0)
+	const [colValues, setColValues] = useState([
+		[0, '%'],
+		[0, '%'],
+		[0, '%'],
+		[0, '%'],
+		[0, '%'],
+		[0, '%'],
+		[0, '%'],
+		[0, '%'],
+		[0, '%'],
+		[0, '%'],
+		[0, '%'],
+		[0, '%'],
+	])
 
 	return (
 		<div className='borders r-c'>
@@ -14,28 +42,52 @@ const DivProperties = () => {
 					<input type='checkbox' onChange={() => setShowRow(!showRow)} />
 					<label>Rows</label>
 					<input
-						style={{ transform: `scale(${showRow ? 1 : 0})` }}
 						type='number'
 						placeholder='No: Rows'
 						min='0'
 						max='12'
+						style={{ transform: `scale(${showRow ? 1 : 0})` }}
+						onChange={e => setRowNum(e.target.value)}
 					/>
 				</div>
-				<div className='rows margins'></div>
+				<div className='rows margins' style={{ display: showRow ? 'block' : 'none' }}>
+					<div className='ínvalue'>
+						<input type='number' />
+						<select>
+							<option>PX</option>
+							<option>VH</option>
+							<option>EM</option>
+							<option>REM</option>
+						</select>
+					</div>
+				</div>
 			</div>
 			<div className='cols-div'>
 				<div className='checkin'>
 					<input type='checkbox' onChange={() => setShowCol(!showCol)} />
 					<label>Columns</label>
 					<input
-						style={{ transform: `scale(${showCol ? 1 : 0})` }}
 						type='number'
 						placeholder='No: Cols'
 						min='0'
 						max='12'
+						style={{ transform: `scale(${showCol ? 1 : 0})` }}
+						onChange={e => setColsNum(e.target.value)}
 					/>
 				</div>
-				<div className='cols margins'></div>
+				<div className='cols margins' style={{ display: showCol ? 'block' : 'none' }}>
+					<div className='ínvalue'>
+						<input type='number' />
+						<select>
+							<option>%</option>
+							<option>VW</option>
+							<option>FR</option>
+							<option>PX</option>
+							<option>EM</option>
+							<option>REM</option>
+						</select>
+					</div>
+				</div>
 			</div>
 		</div>
 	)
