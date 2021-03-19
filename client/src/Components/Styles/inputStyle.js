8e2212a736e93f1412f2ae85941016aad3a475b2
Rehan Ali Shah <rehan.shah70@yahoo.com>
2021-03-19 15:23:52 +0500

    inputStyle added for input style

---
+++ client/src/Components/Styles/inputStyle.js
@@ -0,0 +1,13 @@
+export const inputStyle = {
+	width: '',
+	height: '',
+	margin: '',
+	marginTop: '',
+	marginLeft: '',
+	marginRight: '',
+	backgroundColor: '',
+	border: '',
+	borderRadius: '',
+	textAlign: '',
+	boxShadow: '',
+}
