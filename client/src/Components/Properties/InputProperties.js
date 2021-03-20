91525861f993086d8b2d7cdf04bd0068c42021d1
Rehan Ali Shah <rehan.shah70@yahoo.com>
2021-03-20 14:55:03 +0500

    Componenet for Input properties added

---
+++ client/src/Components/Properties/InputProperties.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const InputProperties = () => {
+	return (
+		<div className='borders r-c'>
+			<p className='second-heading'>Input Properties</p>
+		</div>
+	)
+}
+
+export default InputProperties
--- client/src/Components/Properties/Specific.js
+++ client/src/Components/Properties/Specific.js
@@ -1,6 +1,7 @@
 import React, { useContext } from 'react'
 import { PageContext } from '../Contexts/PageContext'
 import DivProperties from './DivProperties'
+import InputProperties from './InputProperties'
 
 const Specific = () => {
 	const { activeElement, activePage, width } = useContext(PageContext)
@@ -8,7 +9,13 @@ const Specific = () => {
 
 	if (activeElement === activePage || !ele) return <div></div>
 
-	return ele.tagName === 'DIV' ? <DivProperties width={width} activeElement={activeElement} /> : <div></div>
+	return ele.tagName === 'DIV' ? (
+		<DivProperties width={width} activeElement={activeElement} />
+	) : ele.tagName === 'INPUT' ? (
+		<InputProperties width={width} activeElement={activeElement} />
+	) : (
+		<div></div>
+	)
 }
 
 export default Specific
