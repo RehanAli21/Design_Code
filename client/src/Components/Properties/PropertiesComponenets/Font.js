785600a86d10217dcc14fcc0e65881f1a32776a8
Rehan Ali Shah <rehan.shah70@yahoo.com>
2021-03-29 20:49:40 +0500

    Font Style added

--- client/src/Components/Properties/PropertiesComponenets/Font.js
+++ client/src/Components/Properties/PropertiesComponenets/Font.js
@@ -28,6 +28,7 @@ const Font = () => {
 	const [font, setFont] = useState('')
 	const [fontSize, setFontSize] = useState('')
 	const [fontWeight, setFontWeight] = useState('')
+	const [fontStyle, setFontStyle] = useState('')
 
 	const [showCustomFontSize, setShowCustomFontSize] = useState(true)
 
@@ -38,27 +39,32 @@ const Font = () => {
 			const fontSizeInput = document.getElementById('btn-fontsize')
 			const fontSizeSelect = document.getElementById('btn-fontsizeselect')
 			const fontWeightSelect = document.getElementById('btn-fontWeight')
+			const fontStyleSelect = document.getElementById('btn-fontStyle')
 
 			if (width < 540) {
 				fontSelect.value = small.fontFamily ? small.fontFamily : 'default'
 				fontSizeInput.value = small.fontSize ? small.fontSize.split('p')[0] : 0
 				fontSizeSelect.value = small.fontSize ? small.fontSize : 'custom'
 				fontWeightSelect.value = small.fontWeight ? small.fontWeight : 'normal'
+				fontStyleSelect.value = small.fontStyle ? small.fontStyle : 'normal'
 			} else if (width < 720) {
 				fontSelect.value = medium.fontFamily ? medium.fontFamily : 'default'
 				fontSizeInput.value = medium.fontSize ? medium.fontSize.split('p')[0] : 0
 				fontSizeSelect.value = medium.fontSize ? medium.fontSize : 'custom'
 				fontWeightSelect.value = medium.fontWeight ? medium.fontWeight : 'normal'
+				fontStyleSelect.value = medium.fontStyle ? medium.fontStyle : 'normal'
 			} else if (width < 970) {
 				fontSelect.value = large.fontFamily ? large.fontFamily : 'default'
 				fontSizeInput.value = large.fontSize ? large.fontSize.split('p')[0] : 0
 				fontSizeSelect.value = large.fontSize ? large.fontSize : 'custom'
 				fontWeightSelect.value = large.fontWeight ? large.fontWeight : 'normal'
+				fontStyleSelect.value = large.fontStyle ? large.fontStyle : 'normal'
 			} else {
 				fontSelect.value = xlarge.fontFamily ? xlarge.fontFamily : 'default'
 				fontSizeInput.value = xlarge.fontSize ? xlarge.fontSize.split('p')[0] : 0
 				fontSizeSelect.value = xlarge.fontSize ? xlarge.fontSize : 'custom'
 				fontWeightSelect.value = xlarge.fontWeight ? xlarge.fontWeight : 'normal'
+				fontStyleSelect.value = xlarge.fontStyle ? xlarge.fontStyle : 'normal'
 			}
 		}
 	}, [width, activeElement, small, large, medium, xlarge])
@@ -156,6 +162,37 @@ const Font = () => {
 		}
 	}, [fontWeight])
 
+	//For changing fontStyle
+	useEffect(() => {
+		if (small && medium && large && xlarge && fontStyle !== '') {
+			if (width < 540) {
+				setProperties(small, setSmall, 'fontStyle', fontStyle)
+				setChangedSmall(true)
+				if (!changedMedium) setProperties(medium, setMedium, 'fontStyle', fontStyle)
+				if (!changedLarge) setProperties(large, setLarge, 'fontStyle', fontStyle)
+				if (!changedXlarge) setProperties(xlarge, setXlarge, 'fontStyle', fontStyle)
+			} else if (width < 720) {
+				setProperties(medium, setMedium, 'fontStyle', fontStyle)
+				setChangedMedium(true)
+				if (!changedSmall) setProperties(small, setSmall, 'fontStyle', fontStyle)
+				if (!changedLarge) setProperties(large, setLarge, 'fontStyle', fontStyle)
+				if (!changedXlarge) setProperties(xlarge, setXlarge, 'fontStyle', fontStyle)
+			} else if (width < 970) {
+				setProperties(large, setLarge, 'fontStyle', fontStyle)
+				setChangedLarge(true)
+				if (!changedSmall) setProperties(small, setSmall, 'fontStyle', fontStyle)
+				if (!changedMedium) setProperties(medium, setMedium, 'fontStyle', fontStyle)
+				if (!changedXlarge) setProperties(xlarge, setXlarge, 'fontStyle', fontStyle)
+			} else {
+				setProperties(xlarge, setXlarge, 'fontStyle', fontStyle)
+				setChangedXlarge(true)
+				if (!changedSmall) setProperties(small, setSmall, 'fontStyle', fontStyle)
+				if (!changedMedium) setProperties(medium, setMedium, 'fontStyle', fontStyle)
+				if (!changedLarge) setProperties(large, setLarge, 'fontStyle', fontStyle)
+			}
+		}
+	}, [fontStyle])
+
 	const setProperties = (obj, setObj, propertyName, property) => {
 		const temp = Object.assign({}, obj)
 		temp[propertyName] = property
@@ -238,6 +275,13 @@ const Font = () => {
 					<option value='normal'>Regular</option>
 					<option value='lighter'>Light</option>
 					<option value='bold'>Bold</option>
+				</select>
+			</div>
+			<div className='two'>
+				<label>Font Style: </label>
+				<select onChange={e => setFontStyle(e.target.value)} id='btn-fontStyle'>
+					<option value='normal'>Normal</option>
+					<option value='italic'>Italic</option>
 				</select>
 			</div>
 		</React.Fragment>
