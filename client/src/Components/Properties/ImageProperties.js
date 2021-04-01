0b5add47ae72f23edaf991d59604fd3f3944a24e
Rehan Ali Shah <rehan.shah70@yahoo.com>
2021-04-01 23:31:30 +0500

    functionality for img to go separate line is working

--- client/src/Components/Properties/ImageProperties.js
+++ client/src/Components/Properties/ImageProperties.js
@@ -1,12 +1,16 @@
 import React, { useEffect, useState, useContext } from 'react'
 import { PageContext } from '../Contexts/PageContext'
+import { PropertiesContext } from '../Contexts/PropertiesContext'
 
 const ImageProperties = () => {
 	const { activePage, activeElement, pages, setPages } = useContext(PageContext)
+	const { small, setSmall, medium, setMedium, large, setLarge, xlarge, setXlarge } = useContext(PropertiesContext)
 
 	const [img, setImg] = useState('')
 	const [alt, setAlt] = useState('')
+	const [separateLine, setSeparateLine] = useState(false)
 
+	//For setting src of img
 	useEffect(() => {
 		if (img !== '') {
 			const temp = Object.assign({}, pages)
@@ -15,6 +19,7 @@ const ImageProperties = () => {
 		}
 	}, [img])
 
+	//For setting alt of img
 	useEffect(() => {
 		if (alt !== '') {
 			const temp = Object.assign({}, pages)
@@ -37,12 +42,37 @@ const ImageProperties = () => {
 		}
 	}
 
+	//For changing display for separate line
+	useEffect(() => {
+		setProperties(small, setSmall, 'display', separateLine ? 'block' : '')
+		setProperties(medium, setMedium, 'display', separateLine ? 'block' : '')
+		setProperties(large, setLarge, 'display', separateLine ? 'block' : '')
+		setProperties(xlarge, setXlarge, 'display', separateLine ? 'block' : '')
+	}, [separateLine])
+
+	const setProperties = (obj, setObj, propertyName, property) => {
+		const temp = Object.assign({}, obj)
+		temp[propertyName] = property
+		setObj(temp)
+	}
+
 	//For setting img
 	const changeImg = e => setImg(URL.createObjectURL(e.target.files[0]))
 
 	return (
 		<div className='borders btn-specific'>
 			<p className='second-heading'>PROPERTIES</p>
+			<div
+				style={{
+					display: 'grid',
+					gridTemplateColumns: '20px 130px auto',
+					marginLeft: '25px',
+					marginTop: '20px',
+					textAlign: 'center',
+				}}>
+				<input onChange={e => setSeparateLine(e.target.checked)} style={{ marginTop: '5px' }} type='checkbox' />
+				<label>On Separate Line</label>
+			</div>
 			<div className='two'>
 				<label>Img: </label>
 				<input type='file' onChange={e => changeImg(e)} />
