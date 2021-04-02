6bacae750c95c8ca24988e295c085986b30c7e7a
Rehan Ali Shah <rehan.shah70@yahoo.com>
2021-04-02 21:14:43 +0500

    Select element insertion in page is working properly

--- client/src/Components/Page.js
+++ client/src/Components/Page.js
@@ -200,6 +200,25 @@ const Page = () => {
 						null
 					)
 				)
+			} else if (e[0] === 'select') {
+				temp.push(
+					React.createElement(
+						e[0],
+						{
+							key: uuid(),
+							id: e[1].id,
+							style:
+								width < 540
+									? e[1].styles.small
+									: width < 720
+									? e[1].styles.medium
+									: width < 960
+									? e[1].styles.large
+									: e[1].styles.xlarge,
+						},
+						e[2].length > 0 ? showElements(e[2]) : null
+					)
+				)
 			}
 		})
 
--- client/src/Components/Toolbar.js
+++ client/src/Components/Toolbar.js
@@ -5,6 +5,7 @@ import Input from './Tools/Input'
 import Button from './Tools/Button'
 import Text from './Tools/Text'
 import Image from './Tools/Image'
+import Select from './Tools/Select'
 
 //This component is for showing list of elemenets,
 //which can be added.
@@ -20,7 +21,7 @@ const Toolbar = () => {
 		//Iterating each child in children array
 		children.forEach(child => {
 			//if parent found by id, then insert element into children
-			if (child[2] && child[1].id === id) {
+			if (child[2] && child[0] !== 'select' && child[1].id === id) {
 				child[2].push(element)
 				return children
 			}
@@ -69,6 +70,14 @@ const Toolbar = () => {
 					uniqueString={uniqueString}
 				/>
 				<Image
+					findAndInsert={findAndInsert}
+					activeElement={activeElement}
+					activePage={activePage}
+					pages={pages}
+					setPages={setPages}
+					uniqueString={uniqueString}
+				/>
+				<Select
 					findAndInsert={findAndInsert}
 					activeElement={activeElement}
 					activePage={activePage}
