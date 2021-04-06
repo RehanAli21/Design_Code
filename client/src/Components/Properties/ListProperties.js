f6180049347ddb3a9e545e02f9e3defa13a89b1a
Rehan Ali Shah <rehan.shah70@yahoo.com>
2021-04-06 11:39:30 +0500

    list type change is working properly

--- client/src/Components/Properties/ListProperties.js
+++ client/src/Components/Properties/ListProperties.js
@@ -1,11 +1,44 @@
-import React, { useState } from 'react'
+import React, { useContext, useEffect, useState } from 'react'
+import { PageContext } from '../Contexts/PageContext'
 
 const ListProperties = () => {
-	const [listType, setListType] = useState('ul')
+	const { activeElement, activePage, pages, setPages } = useContext(PageContext)
+
+	const [listType, setListType] = useState('')
+
+	//For changing type of list
+	useEffect(() => {
+		if (listType !== '') {
+			const temp = Object.assign({}, pages)
+			findAndChange(temp[activePage], 'type', listType)
+			setPages(temp)
+		}
+	}, [listType])
+
+	//For finding element and changing attribute value
+	const findAndChange = (arr, attribute, changedValue) => {
+		for (let i = 0; i < arr.length; i++) {
+			if (arr[i][1].id === activeElement) {
+				arr[i][1][attribute] = changedValue
+				return true
+			} else if (arr[i][2]) {
+				if (findAndChange(arr[i][2], attribute, changedValue)) {
+					return true
+				}
+			}
+		}
+	}
 
 	return (
 		<div className='borders btn-specific'>
 			<p className='second-heading'>List Properties</p>
+			<div className='two'>
+				<label>Type: </label>
+				<select onChange={e => setListType(e.target.value)}>
+					<option value='ul'>Unorder List</option>
+					<option value='ol'>Ordered List</option>
+				</select>
+			</div>
 		</div>
 	)
 }
