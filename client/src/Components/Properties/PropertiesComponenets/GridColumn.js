24180ef2e2c29c526a7a15aa31d66f35d2f715f9
Rehan Ali Shah <rehan.shah70@yahoo.com>
2021-04-11 20:37:09 +0500

    Grid Column is working

--- client/src/Components/Properties/PropertiesComponenets/GridColumn.js
+++ client/src/Components/Properties/PropertiesComponenets/GridColumn.js
@@ -3,59 +3,59 @@ import { PageContext } from '../../Contexts/PageContext'
 import { PropertiesContext } from '../../Contexts/PropertiesContext'
 
 const GridColumn = () => {
-	// const {
-	// 	small,
-	// 	setSmall,
-	// 	medium,
-	// 	setMedium,
-	// 	large,
-	// 	setLarge,
-	// 	xlarge,
-	// 	setXlarge,
-	// 	changedSmall,
-	// 	setChangedSmall,
-	// 	changedMedium,
-	// 	setChangedMedium,
-	// 	changedLarge,
-	// 	setChangedLarge,
-	// 	changedXlarge,
-	// 	setChangedXlarge,
-	// } = useContext(PropertiesContext)
-	// const { width, activeElement } = useContext(PageContext)
+	const {
+		small,
+		setSmall,
+		medium,
+		setMedium,
+		large,
+		setLarge,
+		xlarge,
+		setXlarge,
+		changedSmall,
+		setChangedSmall,
+		changedMedium,
+		setChangedMedium,
+		changedLarge,
+		setChangedLarge,
+		changedXlarge,
+		setChangedXlarge,
+	} = useContext(PropertiesContext)
+	const { width, activeElement } = useContext(PageContext)
 
 	const [colNum, setColNum] = useState('')
 	let cols = 0
 
-	// useEffect(() => {
-	// 	if (small && medium && large && xlarge && cols !== 0) {
-	// 		const gCol = colNum === 0 ? '' : `${colNum}/${colNum}`
-	// 		if (width < 540) {
-	// 			setProperties(small, setSmall, 'gridColumn', gCol)
-	// 			setChangedSmall(true)
-	// 			if (!changedMedium) setProperties(medium, setMedium, 'gridColumn', gCol)
-	// 			if (!changedLarge) setProperties(large, setLarge, 'gridColumn', gCol)
-	// 			if (!changedXlarge) setProperties(xlarge, setXlarge, 'gridColumn', gCol)
-	// 		} else if (width < 720) {
-	// 			setProperties(medium, setMedium, 'gridColumn', gCol)
-	// 			setChangedMedium(true)
-	// 			if (!changedSmall) setProperties(small, setSmall, 'gridColumn', gCol)
-	// 			if (!changedLarge) setProperties(large, setLarge, 'gridColumn', gCol)
-	// 			if (!changedXlarge) setProperties(xlarge, setXlarge, 'gridColumn', gCol)
-	// 		} else if (width < 970) {
-	// 			setProperties(large, setLarge, 'gridColumn', gCol)
-	// 			setChangedLarge(true)
-	// 			if (!changedSmall) setProperties(small, setSmall, 'gridColumn', gCol)
-	// 			if (!changedMedium) setProperties(medium, setMedium, 'gridColumn', gCol)
-	// 			if (!changedXlarge) setProperties(xlarge, setXlarge, 'gridColumn', gCol)
-	// 		} else {
-	// 			setProperties(xlarge, setXlarge, 'gridColumn', gCol)
-	// 			setChangedXlarge(true)
-	// 			if (!changedSmall) setProperties(small, setSmall, 'gridColumn', gCol)
-	// 			if (!changedMedium) setProperties(medium, setMedium, 'gridColumn', gCol)
-	// 			if (!changedLarge) setProperties(large, setLarge, 'gridColumn', gCol)
-	// 		}
-	// 	}
-	// }, [colNum])
+	useEffect(() => {
+		if (small && medium && large && xlarge && cols !== 0) {
+			const gCol = colNum === 0 ? '' : `${colNum}/${colNum}`
+			if (width < 540) {
+				setProperties(small, setSmall, 'gridColumn', gCol)
+				setChangedSmall(true)
+				if (!changedMedium) setProperties(medium, setMedium, 'gridColumn', gCol)
+				if (!changedLarge) setProperties(large, setLarge, 'gridColumn', gCol)
+				if (!changedXlarge) setProperties(xlarge, setXlarge, 'gridColumn', gCol)
+			} else if (width < 720) {
+				setProperties(medium, setMedium, 'gridColumn', gCol)
+				setChangedMedium(true)
+				if (!changedSmall) setProperties(small, setSmall, 'gridColumn', gCol)
+				if (!changedLarge) setProperties(large, setLarge, 'gridColumn', gCol)
+				if (!changedXlarge) setProperties(xlarge, setXlarge, 'gridColumn', gCol)
+			} else if (width < 970) {
+				setProperties(large, setLarge, 'gridColumn', gCol)
+				setChangedLarge(true)
+				if (!changedSmall) setProperties(small, setSmall, 'gridColumn', gCol)
+				if (!changedMedium) setProperties(medium, setMedium, 'gridColumn', gCol)
+				if (!changedXlarge) setProperties(xlarge, setXlarge, 'gridColumn', gCol)
+			} else {
+				setProperties(xlarge, setXlarge, 'gridColumn', gCol)
+				setChangedXlarge(true)
+				if (!changedSmall) setProperties(small, setSmall, 'gridColumn', gCol)
+				if (!changedMedium) setProperties(medium, setMedium, 'gridColumn', gCol)
+				if (!changedLarge) setProperties(large, setLarge, 'gridColumn', gCol)
+			}
+		}
+	}, [colNum])
 
 	const setProperties = (obj, setObj, propertyName, property) => {
 		const temp = Object.assign({}, obj)
