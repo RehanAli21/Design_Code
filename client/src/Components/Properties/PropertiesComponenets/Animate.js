2cd6b6e6cab4b18a9beca0f94c1ef25a32e45c27
Rehan Ali Shah <rehan.shah70@yahoo.com>
2021-04-17 17:59:58 +0500

    animate comp created for animations

---
+++ client/src/Components/Properties/PropertiesComponenets/Animate.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Animate = () => {
+	return (
+		<div className='borders btn-specific'>
+			<p className='second-heading'>Animations</p>
+		</div>
+	)
+}
+
+export default Animate
