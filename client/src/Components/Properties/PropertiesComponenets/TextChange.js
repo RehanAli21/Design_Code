8215d45e4e80bcf24d2f1f43385b0bdcad5a85ed
Rehan Ali Shah <rehan.shah70@yahoo.com>
2021-04-17 14:06:04 +0500

    Bug fixed in TextChange comp

--- client/src/Components/Properties/PropertiesComponenets/TextChange.js
+++ client/src/Components/Properties/PropertiesComponenets/TextChange.js
@@ -8,23 +8,20 @@ const TextChange = ({ type, display }) => {
 
 	//For setting default values of button text
 	useEffect(() => {
-		const textInput = document.getElementById('btn-textInput')
-		const textarea = document.getElementById('btn-texttextarea')
-
-		const value = findAndReturnText(pages[activePage])
-
-		if (value) {
-			if (textInput) textInput.value = value
-			else if (textarea) textarea.value = value
-		}
+		findAndReturnText(pages[activePage])
 	})
 	//for find element and return button text
 	const findAndReturnText = arr => {
+		const textInput = document.getElementById('btn-textInput')
+		const textarea = document.getElementById('btn-texttextarea')
+
 		for (let i = 0; i < arr.length; i++) {
 			if (arr[i][1].id === activeElement) {
-				return arr[i][1].text
+				if (textInput) textInput.value = arr[i][1].text
+				else if (textarea) textarea.value = arr[i][1].text
+				return true
 			} else if (arr[i][2]) {
-				findAndReturnText(arr[i][2])
+				if (findAndReturnText(arr[i][2])) return true
 			}
 		}
 	}
