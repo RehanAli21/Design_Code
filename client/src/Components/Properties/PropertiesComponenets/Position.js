9a74827334c772e4d61000ea3f8e7629805b9f9b
Rehan Ali Shah <rehan.shah70@yahoo.com>
2021-04-22 03:19:14 +0500

    states added for position comp

--- client/src/Components/Properties/PropertiesComponenets/Position.js
+++ client/src/Components/Properties/PropertiesComponenets/Position.js
@@ -1,12 +1,23 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const Position = () => {
+	const [postion, setPosition] = useState('')
+	const [top, setTop] = useState('')
+	const [topUnit, setTopUnit] = useState('')
+	const [bottom, setBottom] = useState('')
+	const [bottomUnit, setBottomUnit] = useState('')
+	const [left, setLeft] = useState('')
+	const [leftUnit, setLeftUnit] = useState('')
+	const [right, setRight] = useState('')
+	const [rightUnit, setRightUnit] = useState('')
+	const [zIndex, setZIndex] = useState('')
+
 	return (
 		<div className='borders btn-specific'>
 			<p className='second-heading'>POSITIONS</p>
 			<div className='two'>
 				<label>Positions</label>
-				<select id='pos-input'>
+				<select onChange={e => setPosition(e.target.value)} id='pos-input'>
 					<option value='static'>Static</option>
 					<option value='static'>Relative</option>
 					<option value='fixed'>Fixed</option>
@@ -16,8 +27,8 @@ const Position = () => {
 			</div>
 			<div className='three'>
 				<label>Top: </label>
-				<input id='pos-top-input' type='number' className='numberinput' />
-				<select id='pos-top-select'>
+				<input onChange={e => setTop(e.target.value)} id='pos-top-input' type='number' className='numberinput' />
+				<select onChange={e => setTopUnit(e.target.value)} id='pos-top-select'>
 					<option value='px'>PX</option>
 					<option value='vh'>VH</option>
 					<option value='%'>%</option>
@@ -26,8 +37,8 @@ const Position = () => {
 			</div>
 			<div className='three'>
 				<label>Bottom: </label>
-				<input id='pos-bottom-input' type='number' className='numberinput' />
-				<select id='pos-bottom-select'>
+				<input onChange={e => setBottom(e.target.value)} id='pos-bottom-input' type='number' className='numberinput' />
+				<select onChange={e => setBottomUnit(e.target.value)} id='pos-bottom-select'>
 					<option value='px'>PX</option>
 					<option value='vh'>VH</option>
 					<option value='%'>%</option>
@@ -36,8 +47,8 @@ const Position = () => {
 			</div>
 			<div className='three'>
 				<label>Left: </label>
-				<input id='pos-left-input' type='number' className='numberinput' />
-				<select id='pos-left-select'>
+				<input onChange={e => setLeft(e.target.value)} id='pos-left-input' type='number' className='numberinput' />
+				<select onChange={e => setLeftUnit(e.target.value)} id='pos-left-select'>
 					<option value='px'>PX</option>
 					<option value='vw'>VW</option>
 					<option value='%'>%</option>
@@ -46,8 +57,8 @@ const Position = () => {
 			</div>
 			<div className='three'>
 				<label>Right: </label>
-				<input id='pos-right-input' type='number' className='numberinput' />
-				<select id='pos-right-select'>
+				<input onChange={e => setRight(e.target.value)} id='pos-right-input' type='number' className='numberinput' />
+				<select onChange={e => setRightUnit(e.target.value)} id='pos-right-select'>
 					<option value='px'>PX</option>
 					<option value='vw'>VW</option>
 					<option value='%'>%</option>
@@ -56,7 +67,13 @@ const Position = () => {
 			</div>
 			<div className='two'>
 				<label>Z-Index: </label>
-				<input id='pos-index-input' type='number' className='numberinput' defaultValue='0' />
+				<input
+					onChange={e => setZIndex(e.target.value)}
+					id='pos-index-input'
+					type='number'
+					className='numberinput'
+					defaultValue='0'
+				/>
 			</div>
 		</div>
 	)
