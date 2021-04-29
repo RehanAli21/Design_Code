84ecfa712049604edab71782aa2e71914c74134c
Rehan Ali Shah <rehan.shah70@yahoo.com>
2021-04-30 01:50:28 +0500

    Component created for Text Color and Font Size change

--- client/src/Components/Properties/HoverStateProperties.js
+++ client/src/Components/Properties/HoverStateProperties.js
@@ -1,12 +1,14 @@
 import React from 'react'
 import HoverAppearance from './PropertiesComponenets/HoverAppearance'
 import HoverExtra from './PropertiesComponenets/HoverExtra'
+import HoverTextFont from './PropertiesComponenets/HoverTextFont'
 
 const HoverStateProperties = ({ state, activePage, activeElement }) => {
 	return (
 		<div style={{ display: activePage !== activeElement && state === 'hover' ? 'block' : 'none' }}>
 			<HoverAppearance />
 			<HoverExtra />
+			<HoverTextFont />
 		</div>
 	)
 }
