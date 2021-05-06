d4a827f49a3c328110ffed49c345f1b0194776b8
Rehan Ali Shah <rehan.shah70@yahoo.com>
2021-05-06 22:52:13 +0500

    default value added for name comp

--- client/src/Components/Properties/PropertiesComponenets/Name.js
+++ client/src/Components/Properties/PropertiesComponenets/Name.js
@@ -6,6 +6,25 @@ const Name = () => {
 
 	const [name, setName] = useState('')
 
+	//for default name
+	useEffect(() => {
+		defaultvalue(pages[activePage])
+	})
+
+	//helper for default value
+	const defaultvalue = arr => {
+		const nameInput = document.getElementById('name-nameInput')
+
+		for (let i = 0; i < arr.length; i++) {
+			if (arr[i][1].id === activeElement) {
+				if (nameInput) nameInput.value = arr[i][1].name
+				return true
+			} else if (arr[i][2]) {
+				if (defaultvalue(arr[i][2])) return true
+			}
+		}
+	}
+
 	//for chaning element name
 	useEffect(() => {
 		if (name !== '') {
@@ -22,7 +41,7 @@ const Name = () => {
 				arr[i][1].name = name
 				return true
 			} else if (arr[i][2]) {
-				changeName(arr[i][2], name)
+				if (changeName(arr[i][2], name)) return true
 			}
 		}
 	}
@@ -30,7 +49,7 @@ const Name = () => {
 	return (
 		<div className='two'>
 			<label>Name: </label>
-			<input type='text' id='name-nameinput' onChange={e => setName(e.target.value)} />
+			<input type='text' id='name-nameInput' onChange={e => setName(e.target.value)} />
 		</div>
 	)
 }
