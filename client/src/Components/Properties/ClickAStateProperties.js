9e7a25b454ebb3a2f011a736e7fff1b6017f00e4
Rehan Ali Shah <rehan.shah70@yahoo.com>
2021-05-13 18:32:53 +0500

    Element name input added in click(adv)

--- client/src/Components/Properties/ClickAStateProperties.js
+++ client/src/Components/Properties/ClickAStateProperties.js
@@ -1,9 +1,16 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const ClickAStateProperties = ({ activePage, activeElement, state }) => {
+	const [name, setName] = useState('')
+
 	return (
-		<div style={{ display: activePage !== activeElement && state === 'clickjs' ? 'block' : 'none' }}>
-			<p>CLICK(ADVANCE) PROPERTIES</p>
+		<div className='btn-specific' style={{ display: activePage !== activeElement && state === 'clickjs' ? 'block' : 'none' }}>
+			<p className='second-heading'>CLICK(ADVANCE) PROPERTIES</p>
+			<div className='two'>
+				<label>Element Name</label>
+				<input type='text' placeholder='Ele name' id='clickjs-name-input' onChange={e => setName(e.target.value)} />
+			</div>
+			<button style={{ padding: '10px 20px' }}>Apply Style</button>
 		</div>
 	)
 }
