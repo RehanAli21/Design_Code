2be1daa871292363491401cfad809b5594672ea5
Rehan Ali Shah <rehan.shah70@yahoo.com>
2021-06-07 00:22:09 +0500

    UI ready for icon properties

--- client/src/Components/Properties/IconProperties.js
+++ client/src/Components/Properties/IconProperties.js
@@ -1,7 +1,9 @@
-import React, { useState } from 'react'
+import React, { useContext, useState } from 'react'
+import { PageContext } from '../Contexts/PageContext'
 import Name from './PropertiesComponenets/Name'
 
 const IconProperties = () => {
+	const { page, setPage, activeElement } = useContext(PageContext)
 	const [icon, setIcon] = useState('')
 
 	return (
@@ -10,7 +12,20 @@ const IconProperties = () => {
 			<Name />
 			<div className='two'>
 				<label>Icon Name</label>
-				<input type='text' id='icon-class-name' placeholder='Enter Icon name' />
+				<input type='text' id='icon-class-name' placeholder='Enter Icon name' onChange={e => setIcon(e.target.value)} />
+			</div>
+			<div style={{ margin: '10px 20px' }}>
+				<a
+					target='_blank'
+					rel='noreferrer'
+					href='https://www.google.com'
+					style={{ textDecoration: 'none', color: 'rgb(32, 144, 220)', fontWeight: 'bold' }}>
+					<i className='bi-arrow-right-circle-fill'></i> For Icons List click on ME
+				</a>
+				<p style={{ marginTop: '10px', textAlign: 'left' }}>
+					<strong>Instructions:</strong> Choice icon from the link above, then copy full name of icon and paste it on
+					icon Name field
+				</p>
 			</div>
 		</div>
 	)
