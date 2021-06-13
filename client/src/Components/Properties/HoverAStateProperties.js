9248bf3d65baf55772cb4e073f2e1336f7bb9fa2
Rehan Ali Shah <rehan.shah70@yahoo.com>
2021-06-13 21:09:58 +0500

    elements adding and selecting added in hoveradv

--- client/src/Components/Properties/HoverAStateProperties.js
+++ client/src/Components/Properties/HoverAStateProperties.js
@@ -6,50 +6,104 @@ import HoverAdvAppearance from './PropertiesComponenets/HoverAdvAppearance'
 import HoverAdvExtra from './PropertiesComponenets/HoverAdvExtra'
 import Tip from './PropertiesComponenets/Tip'
 
+let counter = 1
 const HoverAStateProperties = () => {
-	const { setHoverTarget, hoverTargetName, setHoverTargetName } = useContext(PropertiesContext)
-	const { width, pages, activePage, activeElement } = useContext(PageContext)
+	const { hoverTargets, setHoverTargets } = useContext(PropertiesContext)
+	const { pages, activePage, activeElement, setMsgBoxMsg, setShowMsgBox } = useContext(PageContext)
 
 	const [name, setName] = useState('')
 	const [showProperties, setShowProperties] = useState(false)
 
 	useEffect(() => {
+		setName('')
 		const nameInput = document.getElementById('hoveradv-name-input')
+		const elementNameSelect = document.getElementById('hoveradv-elementNames-select')
 
-		if (nameInput) {
-			nameInput.value = hoverTargetName
-		}
-
-		if (hoverTargetName !== '') setShowProperties(true)
-		else if (hoverTargetName === '') setShowProperties(false)
-	}, [activeElement, width, hoverTargetName])
-
-	useEffect(() => {
-		if (name !== '') {
-			const nameFound = NameFinder(pages[activePage])
-
-			if (nameFound) {
-				setShowProperties(true)
-				setHoverTargetName(name)
-			} else if (!nameFound) {
-				setShowProperties(false)
-				setHoverTarget('')
-				setHoverTargetName('')
+		for (const e in hoverTargets) {
+			if (hoverTargets[e].selected) {
+				//setting current selected element's name
+				nameInput.value = e
+				elementNameSelect.value = e
+				//if name is legit, then show properties
+				const nameFound = NameFinder(pages[activePage], e, 'notId')
+				setShowProperties(nameFound)
 			}
 		}
-	}, [name])
+	}, [hoverTargets])
 
-	const NameFinder = arr => {
+	const changeName = () => {
+		if (name !== '') {
+			for (const e in hoverTargets) {
+				if (e === name) {
+					setMsgBoxMsg(`${name} is already selected, change it`)
+					setShowMsgBox(true)
+					return
+				}
+			}
+
+			const nameFound = NameFinder(pages[activePage], name, 'id')
+			setShowProperties(nameFound)
+		}
+	}
+
+	const NameFinder = (arr, name, id) => {
 		for (let i = 0; i < arr.length; i++) {
 			if (arr[i][1].name === name) {
 				if (arr[i][0] !== 'option') {
-					setHoverTarget(arr[i][1].id)
+					if (id === 'id') {
+						const temp = {}
+						for (const e in hoverTargets) {
+							if (hoverTargets[e].selected) {
+								temp[name] = hoverTargets[e]
+								temp[name].id = arr[i][1].id
+							} else {
+								temp[e] = hoverTargets[e]
+							}
+						}
+						setHoverTargets(temp)
+					}
 					return true
 				}
 			} else if (arr[i][2]) {
-				if (NameFinder(arr[i][2])) return true
+				if ((NameFinder(arr[i][2]), name, id) === true) return true
 			}
 		}
+		return false
+	}
+
+	const printElementsNames = () => {
+		const arr = []
+		for (const e in hoverTargets) {
+			arr.push(
+				<option key={e} value={e}>
+					{e}
+				</option>
+			)
+		}
+
+		return arr
+	}
+
+	const addElement = () => {
+		const temp = Object.assign({}, hoverTargets)
+
+		for (const e in temp) temp[e].selected = false
+
+		temp[`H${counter}`] = {
+			selected: true,
+			style: {},
+			id: '',
+		}
+		counter++
+		setHoverTargets(temp)
+	}
+
+	const selectElement = e => {
+		const temp = Object.assign({}, hoverTargets)
+
+		for (const ele in temp) temp[ele].selected = ele === e.target.value
+
+		setHoverTargets(temp)
 	}
 
 	return (
@@ -64,9 +118,33 @@ const HoverAStateProperties = () => {
 				/>
 				HOVER ELEMENT PROPERTIES
 			</p>
-			<div className='two'>
-				<label>Element Name</label>
+			<div
+				style={{
+					display: 'grid',
+					gridTemplateColumns: '70px 90px 50px',
+					columnGap: '10px',
+					margin: '10px 22px',
+				}}>
+				<label style={{ paddingTop: '5px' }}>Elements</label>
+				<select onChange={selectElement} id='hoveradv-elementNames-select'>
+					{printElementsNames()}
+				</select>
+				<button style={{ padding: '5px 10px' }} onClick={addElement}>
+					Add
+				</button>
+			</div>
+			<div
+				style={{
+					display: Object.keys(hoverTargets).length > 0 ? 'grid' : 'none',
+					gridTemplateColumns: '70px 90px 60px',
+					columnGap: '10px',
+					margin: '10px 22px',
+				}}>
+				<label style={{ paddingTop: '5px' }}>Name: </label>
 				<input type='text' placeholder='Ele name' id='hoveradv-name-input' onChange={e => setName(e.target.value)} />
+				<button onClick={changeName} style={{ padding: '5px 10px' }}>
+					Apply
+				</button>
 			</div>
 			<div style={{ display: showProperties ? 'block' : 'none' }}>
 				<HoverAdvOne />
