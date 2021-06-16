837e01ffa8e667401c59656d653150d6a5c5d997
Rehan Ali Shah <rehan.shah70@yahoo.com>
2021-06-16 21:33:16 +0500

    font adding completed

--- client/src/Components/Contexts/TemplateContext.js
+++ client/src/Components/Contexts/TemplateContext.js
@@ -1,4 +1,4 @@
-import React, { useState, createContext } from 'react'
+import React, { useState, createContext, useEffect } from 'react'
 
 export const TemplateContext = createContext()
 
@@ -7,6 +7,30 @@ export const TemplateProvider = props => {
 	const [fontSizes, setFontSizes] = useState({})
 	const [fonts, setFonts] = useState([])
 	const [showTemplate, setShowTemplate] = useState(true)
+
+	useEffect(() => {
+		if (fonts.length > 0) {
+			let font = ''
+			fonts.forEach(e => {
+				let name = ''
+				if (e.search(' ') !== -1) {
+					const a = e.split(' ')
+					a.forEach(i => {
+						name += `${i.charAt(0).toUpperCase() + i.slice(1, i.length).substr(0, i.length - 1)}+`
+					})
+					name = name.substr(0, name.length - 1)
+					console.log(name)
+				} else {
+					name = e.charAt(0).toUpperCase() + e.slice(1, e.length)
+				}
+				font += `family=${name}&`
+			})
+			const link = document.getElementById('googleFonts')
+			if (link) {
+				link.href = `https://fonts.googleapis.com/css2?${font.substr(0, font.length - 1)}`
+			}
+		}
+	}, [fonts])
 
 	return (
 		<TemplateContext.Provider
